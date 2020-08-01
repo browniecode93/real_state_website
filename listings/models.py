d7a83071d645f8d194be1691fec6f05b7995e796
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2020-08-01 14:47:30 +0430

    use decimal for lot_size

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-from realtors.model import Realtor
+from realtors.models import Realtor
 
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
@@ -15,7 +15,7 @@ class Listing(models.Model):
     bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
-    lot_size = models.CharField(max_digits=5, decimal_places=1)
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d')
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
